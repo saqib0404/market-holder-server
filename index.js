2da2b70ca3761030cc73b91d01f5f6e60e15b2e8
Saqib Ahmad <you@example.com>
2022-10-26 10:38:28 +0600

    courses data made and route declared

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const cors = require('cors');
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send("Server is running");
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
 })
 
 app.listen(port, () => {
