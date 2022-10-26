34f2bd632b4eee062db800187d3c3bdd750bfaea
Saqib Ahmad <you@example.com>
2022-10-26 10:18:40 +0600

    server for markt holder is primarily setted

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send("Server is running");
+})
+
+app.listen(port, () => {
+    console.log('server is running on ', port);
+})
