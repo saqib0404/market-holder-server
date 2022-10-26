8881f30d869aa2a725774f3a91ad273f97883126
Saqib Ahmad <you@example.com>
2022-10-26 10:22:49 +0600

    cross origin resource sharing is added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
+const cors = require('cors');
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Server is running");
